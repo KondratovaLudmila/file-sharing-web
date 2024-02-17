c03e971787ea7cd84884de98994644738ef0318c
KarynaNester <liubatskakarina@gmail.com>
2024-02-17 12:20:07 +0200

    change user

--- src/models/user.py
+++ src/models/user.py
@@ -1,15 +1,23 @@
-from sqlalchemy import Column, Integer, DateTime, func, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, DateTime, func, String, Boolean
 
-#required import for each table
+
 from .base import Base
+
+
 
 class User(Base):
     __tablename__ = "users"
 
-    #required fields for each table
+    # required fields for each table
     id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(150), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-    
-    
+    confirmed = Column(Boolean, default=False)
+    ban = Column(Boolean, default=False)
+
+
