5420aa9515289b4b2742aa7f8c188092035e661d
litvinushka <100357494+litvinushka@users.noreply.github.com>
2024-02-21 15:45:02 +0200

    updatet users

--- src/routes/users.py
+++ src/routes/users.py
@@ -3,51 +3,59 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from ..schemas.user import UserCreate, UserUpdate, User
-from ..repository.users import UserRepository  # Зміна імені класу
+from ..repository.users import UserRepository
 from ..dependencies.db import get_db
 
 router = APIRouter(prefix='/users', tags=["users"])
 
+
+@router.get('/', response_model=List[User])
+async def get_users(query: str = Query(..., min_length=1), db: Session = Depends(get_db)):
+    user_repo = UserRepository(db)
+    users = await user_repo.get_many(query)
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Users not found")
+    return users
+
+
 @router.post('/', response_model=User, status_code=status.HTTP_201_CREATED)
 async def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
-    user = UserRepository(db).create_user(**user_data.dict())  # Виклик методу create_user з правильними аргументами
+    user = await UserRepository(db).create(**user_data.dict())
     return user
 
 
 @router.put('/{user_id}', response_model=User)
 async def update_user(user_id: int, user_data: UserUpdate, db: Session = Depends(get_db)):
     user_repo = UserRepository(db)
-    user = await user_repo.get_single_user(user_id=user_id)
+    user = await user_repo.get_single(user_id=user_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
-    updated_user = await user_repo.update_user(user=user, **user_data.dict())
+    updated_user = await user_repo.update(user=user, **user_data.dict())
     return updated_user
 
 
 @router.delete('/{user_id}', response_model=User)
 async def delete_user(user_id: int, db: Session = Depends(get_db)):
     user_repo = UserRepository(db)
-    user = await user_repo.get_single_user(user_id=user_id)
+    user = await user_repo.get_single(user_id=user_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    user_repo.delete_user(user)
+    await user_repo.delete(user)
     return user
-
 
 @router.get('/{user_id}', response_model=User)
 async def get_user(user_id: int, db: Session = Depends(get_db)):
-    user = UserRepository(db).get_single_user(user_id=user_id)
+    user = await UserRepository(db).get_single(user_id=user_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
-
-@router.get('/search/', response_model=List[User])
-async def search_users(query: str = Query(..., min_length=1), db: Session = Depends(get_db)):
+@router.get('/users/{username}', response_model=User)
+async def get_user_by_username(username: str, db: Session = Depends(get_db)):
     user_repo = UserRepository(db)
-    users = user_repo.search_users(query)
-    if not users:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Users not found")
-    return users
+    user = await user_repo.get_username(username)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
