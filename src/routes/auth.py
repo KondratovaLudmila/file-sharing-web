a7e3ead393f0e3976ed7b8273bff95ec64b805ec
litvinushka <100357494+litvinushka@users.noreply.github.com>
2024-02-21 14:12:17 +0200

    added auth.py
    routes/auth
    services/auth
    updated requirements

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,9 +1,9 @@
 from fastapi import Depends, APIRouter, HTTPException, status
-
 from sqlalchemy.orm import Session
 
 from ..dependencies.db import SessionLocal, get_db
 from ..services.auth import (oauth2_scheme,
+                             create_refresh_token,
                              create_access_token, 
                              get_user_by_refresh_token, 
                              verify_password)
@@ -14,29 +14,33 @@ router = APIRouter(prefix='/auth', tags=["auth"])
 
 @router.post('/signup', response_model=User, status_code=status.HTTP_201_CREATED)
 async def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
-    user_repo = UserRepository(db)
-    user = await user_repo.create(**user_data.dict())
+    user = await UserRepository(db).create(**user_data.dict())
     return user
 
-@router.post("/signin")
+
+@router.post("/signin", response_model=dict)
 async def signin(username: str, password: str, db: Session = Depends(get_db)):
-    user_repo = UserRepository(db)
-    user = user_repo.get_user(username)
+    user = UserRepository.get_user(db, username)
     
-    if not user or not user_repo.verify_password(password, user.password):
+    if not user or not verify_password(password, user.password):
         raise HTTPException(status_code=401, detail="Incorrect username or password")
+
     
-    refresh_token = user_repo.create_refresh_token(user)
-    
-    return {"access_token": create_access_token(data={"sub": username}),
-            "refresh_token": refresh_token,
-            "token_type": "bearer"}
+    refresh_token = create_refresh_token({"sub": username})
+
+    user_repo = UserRepository(db)
+    user_repo.update(user, refresh_token=refresh_token)
+
+    return {
+        "access_token": create_access_token(data={"sub": username}),
+        "refresh_token": refresh_token,
+        "token_type": "bearer"
+    }
+
 
 @router.post("/refresh_token")
-async def refresh_token(refresh_token: str = Depends(oauth2_scheme), db: Session = Depends(SessionLocal)):
-    user_repo = UserRepository(db)
-    user = await user_repo.get_user_by_refresh_token(refresh_token)
-    
+async def refresh_token(refresh_token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    user = await get_user_by_refresh_token(refresh_token, db)
     if not user:
         raise HTTPException(status_code=401, detail="Invalid refresh token")
 
--- src/services/auth.py
+++ src/services/auth.py
@@ -3,10 +3,11 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt, ExpiredSignatureError
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
-from ..dependencies.db import SessionLocal
+
+from ..dependencies.db import get_db, SessionLocal
 from ..models.user import User, Role
 from ..conf.config import settings  
-from ..routes.auth import router as auth
+from ..repository.users import UserRepository
 
 ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -23,13 +24,18 @@ def create_jwt_token(data: dict):
 def verify_password(plain_password, hashed_password):
     return plain_password == hashed_password
 
-def get_user(db: Session, username: str):
-    return db.query(User).filter(User.name == username).first()
 
 def create_access_token(data: dict):
     return create_jwt_token(data)
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(SessionLocal)):
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=7)
+    to_encode.update({"exp": expire})
+    encoded_refresh_token = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
+    return encoded_refresh_token
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -43,14 +49,9 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     except JWTError:
         raise credentials_exception
 
-    return get_user(db, username)
+    return UserRepository.get_username(username)(db, username)
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.role == Role.user:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-async def get_user_by_refresh_token(refresh_token: str, db: Session = Depends(SessionLocal)):
+async def get_user_by_refresh_token(refresh_token: str, db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate refresh token",
@@ -64,4 +65,4 @@ async def get_user_by_refresh_token(refresh_token: str, db: Session = Depends(Se
     except ExpiredSignatureError:
         raise credentials_exception
 
-    return get_user(db, username)
+    return UserRepository.get_username(username)(db, username)
